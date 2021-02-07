d638d1199eb9bbef5848cf550a31c3523c60f244
mohit4 <mohitkumar2801@gmail.com>
2021-02-07 18:36:40 +0530

    Added automatron script

---
+++ snippets/base/globals.py
@@ -0,0 +1,11 @@
+JOB_NAME = 'MainSnippet'
+INPUT_DIR_NAME = 'input'
+OUTPUT_DIR_NAME = 'output'
+LOG_DIR_NAME = 'logs'
+LOGGING_LOG_FORMAT = "%(asctime)s [ %(levelname)s ] : %(message)s"
+LOGGING_DATE_FORMAT = "%Y%m%d %H:%M:%S"
+
+CONFIG_FILE_NAME = 'config.yaml'
+LOGGING_LEVEL = logging.DEBUG
+CLI_ARGS = None
+LOGGER = None
---
+++ snippets/base/imports.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import logging
+import argparse
+import yaml
+from datetime import datetime
